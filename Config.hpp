0ae64d61b85522041ef3b6585b4ea35354c01f25
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-12 11:57:10 +0800

    Create Config.hpp

---
+++ Config.hpp
@@ -0,0 +1,10 @@
+#ifndef __LOCK_CONFIG_HPP__
+#define __LOCK_CONFIG_HPP__
+
+#define WORK_LOAD 1 << 8
+#define NUM_THREADS 1 << 4
+#define NUM_SLOTS 1 << 4
+#define NUM_RUNS 1 << 2
+#define NUM_REENTRIES 1 << 22
+
+#endif
