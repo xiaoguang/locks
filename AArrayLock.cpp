bd8609374190d35657262e67149835377123ca6a
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-14 20:11:16 +0800

    Update AArrayLock.cpp

--- AArrayLock.cpp
+++ AArrayLock.cpp
@@ -9,8 +9,8 @@ AArrayLock::AArrayLock(uint64_t size) {
   _ticket = 0;
   _flags = new ALIGNED[_size];
   for(int i = 0; i < _size; i++) {
-    if(i == 0) _flags[i].flag = true;
-    else _flags[i].flag = false;
+    if(i == 0) _flags[i]._flag = true;
+    else _flags[i]._flag = false;
   }
 }
 
@@ -21,13 +21,13 @@ AArrayLock::~AArrayLock() {
 void AArrayLock::lock() {
   uint64_t slot = __atomic_fetch_add(&_ticket, 1, __ATOMIC_RELAXED);
   _ME = slot % _size;
-  while(!_flags[_ME].flag) {}
+  while(!_flags[_ME]._flag) {}
 }
 
 void AArrayLock::unlock() {
   uint64_t slot = (_ME + 1) % _size;
-  _flags[_ME].flag = false;
-  _flags[slot].flag = true;
+  _flags[_ME]._flag = false;
+  _flags[slot]._flag = true;
 }
 
 }
