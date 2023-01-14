3f354373595eead927458443615c2f47cfe73058
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-14 20:13:13 +0800

    Update Config.hpp

--- Config.hpp
+++ Config.hpp
@@ -1,10 +1,22 @@
 #ifndef __LOCK_CONFIG_HPP__
 #define __LOCK_CONFIG_HPP__
 
-#define WORK_LOAD 1 << 8
+#define WORK_LOAD 1 << 10
 #define NUM_THREADS 1 << 4
 #define NUM_SLOTS 1 << 4
 #define NUM_RUNS 1 << 2
-#define NUM_REENTRIES 1 << 22
+#define NUM_REENTRIES 1 << 12
+
+#include <type_traits>
+
+using std::aligned_storage;
+using std::alignment_of;
+
+namespace lock {
+
+typedef aligned_storage<63, alignment_of<bool>::value>::type Padding_63;
+typedef aligned_storage<62, alignment_of<bool>::value>::type Padding_62;
+
+}
 
 #endif
