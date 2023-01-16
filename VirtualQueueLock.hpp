83566ced32adab0634ec6274d4dbfbebf2a26a4b
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-16 11:53:03 +0800

    Update VirtualQueueLock.hpp

--- VirtualQueueLock.hpp
+++ VirtualQueueLock.hpp
@@ -1,20 +1,13 @@
 #ifndef __VIRTUAL_QUEUE_LOCK_HPP__
 #define __VIRTUAL_QUEUE_LOCK_HPP__
 
-#include <type_traits>
-#include <time.h>
 #include "Lock.hpp"
-
-using std::aligned_storage;
-using std::alignment_of;
 
 namespace lock {
 
-typedef aligned_storage<63, alignment_of<bool>::value>::type Align;
-
 struct VQNode {
   VQNode() { _locked = true; }
-  Align _padding;
+  Padding_63 _padding;
   bool _locked;
 };
 
