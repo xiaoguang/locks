d6a9a053e7faf9c00e3e906c614733edf782429a
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-16 11:46:35 +0800

    Create MCSQueueLock.hpp

---
+++ MCSQueueLock.hpp
@@ -0,0 +1,38 @@
+
+#ifndef __MCS_QUEUE_LOCK_HPP__
+#define __MCS_QUEUE_LOCK_HPP__
+
+#include "Lock.hpp"
+#include "MutexLock.hpp"
+
+namespace lock {
+
+struct MCSQNode {
+  MCSQNode() {
+    _locked = true;
+    _next = nullptr;
+  }
+  Padding_54 _padding;
+  bool _locked;
+  MCSQNode* _next;
+};
+
+class MCSQueueLock : public BaseLock {
+ public:
+  static __thread MCSQNode* _CURR;
+
+  explicit MCSQueueLock();
+  ~MCSQueueLock();
+
+  virtual void lock();
+  virtual void unlock();
+
+ private:
+  MCSQNode* _tail;
+  MCSQueueLock(MCSQueueLock&);
+  MCSQueueLock& operator=(MCSQueueLock&);
+};
+
+}
+
+#endif
