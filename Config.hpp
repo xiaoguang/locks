e3d3acd7e58990d0d083868780d6a66d95047517
xiaoguang ye <xiaoguang4ever@gmail.com>
2023-01-16 07:35:03 -0800

    fix makefile

--- Config.hpp
+++ Config.hpp
@@ -4,8 +4,8 @@
 #define __LOCK_CONFIG_HPP__
 
 #define WORK_LOAD 1 << 10
-#define NUM_THREADS 1 << 4
-#define NUM_SLOTS 1 << 4
+#define NUM_THREADS 6
+#define NUM_SLOTS 6
 #define NUM_RUNS 1 << 2
 #define NUM_REENTRIES 1 << 15
 
