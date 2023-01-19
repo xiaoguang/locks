f52f0e981e5443704d8d34e7f2f8e8022c16ecf0
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-19 18:19:08 +0800

    Update Stats.hpp

--- Stats.hpp
+++ Stats.hpp
@@ -8,14 +8,16 @@ namespace lock {
 struct Stats {
   Stats(pthread_t owner) {
     _owner = owner;
-    _workTicks = 0;
-    _lockTicks = 0;
+    _totalTicks  = 0;
+    _workTicks   = 0;
+    _lockTicks   = 0;
     _unlockTicks = 0;
   }
 
   ~Stats() {}
 
   pthread_t _owner;
+  TicksClock::Ticks _totalTicks;
   TicksClock::Ticks _workTicks;
   TicksClock::Ticks _lockTicks;
   TicksClock::Ticks _unlockTicks;
