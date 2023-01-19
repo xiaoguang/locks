eb58935e66ae56284e78815daa970b94033d0504
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-19 18:18:04 +0800

    Update runner.cpp

--- runner.cpp
+++ runner.cpp
@@ -4,19 +4,13 @@
 #include "Thread.hpp"
 #include <queue>
 #include <string>
+#include <cassert>
 
 using lock::LockBenchmark;
 using lock::makeThread;
 using std::tr1::bind;
 using std::queue;
 using std::string;
-
-struct TestDetails {
-  string _lockName;
-  lock::BaseLock* _lock;
-  int _numberOfThreads;
-  pthread_t* _threads;
-};
 
 void timeDiff(
   const struct timespec & start,
@@ -33,153 +27,181 @@ void timeDiff(
   }
 }
 
-void TEST0() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    TicketLock l;
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    struct timespec start, stop, diff;
-    clock_gettime(CLOCK_REALTIME, &start);
-    TicksClock::Ticks before = TicksClock::getTicks();
-
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-    clock_gettime(CLOCK_REALTIME, &stop);
-
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    timeDiff(start, stop, diff);
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-    cout << diff.tv_sec << " : " << diff.tv_nsec << endl;
+struct TestDetails {
+  TestDetails(string name, lock::BaseLock* lock, bool metrics) {
+    _name = name;
+    _lock = lock;
+    _metrics = metrics;
   }
-  cout << endl;
+
+  ~TestDetails() { delete _lock; }
+
+  string _name;
+  lock::BaseLock* _lock;
+  bool _metrics;
+};
+
+uint64_t correctnessExpected = (uint64_t)WORK_LOAD * NUM_THREADS * (uint64_t)NUM_REENTRIES;
+uint64_t metricsExpected = (uint64_t)METRICS_LOAD * NUM_THREADS * (uint64_t)METRICS_REENTRIES;
+queue<TestDetails*> correctness;
+queue<TestDetails*> metrics;
+
+void TESTS() {
+  {
+    SpinLock* l = new SpinLock();
+    TestDetails* t = new TestDetails("spin_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    TTASSpinLock* l = new TTASSpinLock();
+    TestDetails* t = new TestDetails("test_tas_spin_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    MutexLock* l = new MutexLock();
+    TestDetails* t = new TestDetails("mutex_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    TicketLock* l = new TicketLock();
+    TestDetails* t = new TestDetails("ticket_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    ArrayLock* l = new ArrayLock(NUM_SLOTS);
+    TestDetails* t = new TestDetails("array_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    AArrayLock* l = new AArrayLock(NUM_SLOTS);
+    TestDetails* t = new TestDetails("aligned_array_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    VirtualQueueLock* l = new VirtualQueueLock;
+    TestDetails* t = new TestDetails("virtual_queue_lock", l, false);
+    correctness.push(t);
+  }
+
+  {
+    MCSQueueLock* l = new MCSQueueLock;
+    TestDetails* t = new TestDetails("mcs_queue_lock", l, false);
+    correctness.push(t);
+  }
+
+
+
+  {
+    SpinLock* l = new SpinLock();
+    TestDetails* t = new TestDetails("spin_lock", l, true);
+    correctness.push(t);
+  }
+
+  {
+    TTASSpinLock* l = new TTASSpinLock();
+    TestDetails* t = new TestDetails("test_tas_spin_lock", l, true);
+    correctness.push(t);
+  }
+
+  {
+    MutexLock* l = new MutexLock();
+    TestDetails* t = new TestDetails("mutex_lock", l, true);
+    metrics.push(t);
+  }
+
+  {
+    TicketLock* l = new TicketLock();
+    TestDetails* t = new TestDetails("ticket_lock", l, true);
+    metrics.push(t);
+  }
+
+  {
+    ArrayLock* l = new ArrayLock(NUM_SLOTS);
+    TestDetails* t = new TestDetails("array_lock", l, true);
+    metrics.push(t);
+  }
+
+  {
+    AArrayLock* l = new AArrayLock(NUM_SLOTS);
+    TestDetails* t = new TestDetails("aligned_array_lock", l, true);
+    metrics.push(t);
+  }
+
+  {
+    VirtualQueueLock* l = new VirtualQueueLock;
+    TestDetails* t = new TestDetails("virtual_queue_lock", l, true);
+    metrics.push(t);
+  }
+
+  {
+    MCSQueueLock* l = new MCSQueueLock;
+    TestDetails* t = new TestDetails("mcs_queue_lock", l, true);
+    metrics.push(t);
+  }
 }
 
-void TEST1() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    MutexLock l;
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    struct timespec start, stop, diff;
-    clock_gettime(CLOCK_REALTIME, &start);
-    TicksClock::Ticks before = TicksClock::getTicks();
+void runTests(queue<TestDetails*>& target) {
+  while (!target.empty()) {
+    TestDetails* details = target.front();
 
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-    clock_gettime(CLOCK_REALTIME, &stop);
+    for(int k = 0; k < NUM_RUNS; k++) {
+      lock::BaseLock* l = details->_lock;
+      LockBenchmark lb(l);
+      pthread_t threads[NUM_THREADS];
+      struct timespec start, stop, diff;
+      clock_gettime(CLOCK_REALTIME, &start);
 
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    timeDiff(start, stop, diff);
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-    cout << diff.tv_sec << " : " << diff.tv_nsec << endl;
+      for(int i = 0; i < NUM_THREADS; i++) {
+        if(details->_metrics) {
+          threads[i] = makeThread(
+            bind(&LockBenchmark::correctness, &lb, METRICS_REENTRIES, METRICS_LOAD, details->_metrics)
+          );
+        }
+        else {
+          threads[i] = makeThread(
+            bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES, WORK_LOAD, details->_metrics)
+          );
+        }
+      }
+      for(int i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
+
+      clock_gettime(CLOCK_REALTIME, &stop);
+      timeDiff(start, stop, diff);
+      cout << "Lock Name    : " << details->_name << endl;
+      cout << "Correctness  : " << lb.count() << endl;
+
+      if(details->_metrics) {
+        cout << "Expected     : " << metricsExpected << endl;
+        assert(lb.count() == metricsExpected);
+        cout << "total  costs : " << lb.totalTicks() << endl;
+        cout << "work   costs : " << lb.workTicks() << endl;
+        cout << "lock   costs : " << lb.lockTicks() << endl;
+        cout << "unlock costs : " << lb.unlockTicks() << endl;
+      }
+      else {
+        cout << "Expected     : " << correctnessExpected << endl;
+        assert(lb.count() == correctnessExpected);
+      }
+
+      cout << "Time Elapse  : " << diff.tv_sec << "." << diff.tv_nsec << endl;
+      cout << endl;
+    }
+    cout << endl;
+
+    delete details;
+    target.pop();
   }
-  cout << endl;
-}
-
-void TEST2() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    ArrayLock l(NUM_SLOTS);
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    TicksClock::Ticks before = TicksClock::getTicks();
-
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-  }
-  cout << endl;
-}
-
-void TEST3() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    AArrayLock l(NUM_SLOTS);
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    TicksClock::Ticks before = TicksClock::getTicks();
-
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-  }
-  cout << endl;
-}
-
-void TEST4() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    VirtualQueueLock l;
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    TicksClock::Ticks before = TicksClock::getTicks();
-
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-  }
-  cout << endl;
-}
-
-void TEST5() {
-  for(int k = 0; k < NUM_RUNS; k++) {
-    MCSQueueLock l;
-    LockBenchmark lb(&l);
-    pthread_t threads[NUM_THREADS];
-    TicksClock::Ticks before = TicksClock::getTicks();
-
-    for(int i = 0; i < NUM_THREADS; i++)
-      threads[i] = makeThread(bind(&LockBenchmark::correctness, &lb, NUM_REENTRIES));
-    for(int i = 0; i < NUM_THREADS; i++)
-      pthread_join(threads[i], NULL);
-
-    TicksClock::Ticks duration = TicksClock::getTicks() - before;
-    cout << "Correctness  : " << lb.count() << endl;
-    cout << "total  costs : " << duration << endl;
-    cout << "work   costs : " << lb.workTicks() << endl;
-    cout << "lock   costs : " << lb.lockTicks() << endl;
-    cout << "unlock costs : " << lb.unlockTicks() << endl;
-  }
-  cout << endl;
 }
 
 int main() {
-  TEST0();
-  TEST1();
-  TEST2();
-  TEST3();
-  TEST4();
-  TEST5();
+  TESTS();
+  runTests(correctness);
+  runTests(metrics);
 }
