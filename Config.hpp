93fa92f2cad520583b3fb5537358b2e5480aaa54
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-19 18:15:08 +0800

    Update Config.hpp

--- Config.hpp
+++ Config.hpp
@@ -3,11 +3,13 @@
 #ifndef __LOCK_CONFIG_HPP__
 #define __LOCK_CONFIG_HPP__
 
-#define WORK_LOAD 1 << 10
-#define NUM_THREADS 6
-#define NUM_SLOTS 6
+#define NUM_REENTRIES 1 << 10
+#define METRICS_REENTRIES 1 << 16
+#define WORK_LOAD 1 << 8
+#define METRICS_LOAD 1 << 12
+#define NUM_THREADS 1 << 4
+#define NUM_SLOTS 1 << 4
 #define NUM_RUNS 1 << 2
-#define NUM_REENTRIES 1 << 15
 
 #include <type_traits>
 
