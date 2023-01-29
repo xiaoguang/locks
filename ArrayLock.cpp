608b229fac2aab3d6a7ce901bf34e603b1fea728
xiaoguang <xiaoguang4ever@gmail.com>
2023-01-29 16:21:56 +0800

    Update ArrayLock.cpp

--- ArrayLock.cpp
+++ ArrayLock.cpp
@@ -19,15 +19,20 @@ ArrayLock::~ArrayLock() {
 }
 
 void ArrayLock::lock() { 
-  uint64_t slot = __atomic_fetch_add(&_ticket, 1, __ATOMIC_RELAXED);
+  uint64_t slot = __atomic_fetch_add(&_ticket, 1, __ATOMIC_ACQUIRE);
   _ME = slot % _size;
   while(!_flags[_ME]) {}
 }
 
 void ArrayLock::unlock() {
   uint64_t slot = (_ME + 1) % _size;
-  _flags[_ME] = false;
-  _flags[slot] = true;
+  
+  // intel chipset has a stronger default protocal
+  // vs. ARM chipset's weaker protocal
+  // _flags[_ME] = false;
+  // _flags[slot] = true;
+  __atomic_store_n(&_flags[_ME], false, __ATOMIC_RELEASE);
+  __atomic_store_n(&_flags[slot], true, __ATOMIC_RELEASE);
 }
 
 }
